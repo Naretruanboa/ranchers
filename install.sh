67ec5496591c95f0143761c783d65e820e149c0e
Phongphan Phienphanich <garpong@gmail.com>
2024-01-22 14:06:28 +0700

    complete install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo Install docker ...
+curl https://releases.rancher.com/install-docker/23.0.3.sh | sh
+
+echo Install docker compose ...
+apt install docker-compose
+
+echo set net bridge ...
+sysctl net.bridge.bridge-nf-call-iptables
+
+echo Install kubectl ...
+snap install kubectl --classic
+
+mkdir /root/rancher
+curl -o docker-compose.yaml https://github.com/kapong/ranchers/raw/main/docker-compose.yml
+
+cd /root/rancher
+docker compose up -d
+
+echo "Please wait 2-3 minutes for the installation to complete."
+
+echo "Bootstrap Password"
+docker logs rancher-center  2>&1 | grep "Bootstrap Password:"
