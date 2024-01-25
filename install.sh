1785c2c75a7da6e65a6d1629b5c9de347164b7b6
Phongphan Phienphanich <garpong@gmail.com>
2024-01-25 10:19:10 +0700

    integrate ipv4-bridge.bash

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
 echo Install docker ...
 curl https://releases.rancher.com/install-docker/23.0.3.sh | sh
 
@@ -17,11 +35,16 @@ mkdir /root/rancher
 
 cd /root/rancher
 wget https://github.com/kapong/ranchers/raw/main/docker-compose.yml
-docker compose up -d
 
-echo "Please wait 2-3 minutes for the installation to complete."
+echo "Done, you should reboot now."
 
-sleep 180
-echo "Bootstrap Password"
+echo "After reboot, run this command to start rancher server:" 
+echo "docker compose up -d\n"
+echo "and"
 echo 'docker logs rancher-center  2>&1 | grep "Bootstrap Password:"'
-docker logs rancher-center  2>&1 | grep "Bootstrap Password:"
+
+# echo "Please wait 2-3 minutes for the installation to complete."
+
+# sleep 180
+# echo "Bootstrap Password"
+# docker logs rancher-center  2>&1 | grep "Bootstrap Password:"
